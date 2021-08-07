dba91e4e37d21e2efc8049c62ad75092353206eb
kamal-pfc <kamaljit.s@pfc-group.com>
2021-08-07 22:21:33 +0530

    kamal ke changes

--- static/js/startexam.js
+++ static/js/startexam.js
@@ -1,5 +1,90 @@
+// function CloseWindow(){
+//   alert("Thank you for visiting W3Schools!");
+// }
+//$(window).unload( function () { alert("Bye now!"); } );
+
+
+// function requestFullScreen(element) {
+//   // Supports most browsers and their versions.
+//   var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
+
+//   if (requestMethod) { // Native full screen.
+//       requestMethod.call(element);
+//   } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+//       var wscript = new ActiveXObject("WScript.Shell");
+//       if (wscript !== null) {
+//           wscript.SendKeys("{F11}");
+//       }
+//   }
+// }
+
+// var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+// if(isChrome){
+  $('#overlay').css('display','none');
+//}
+//console.log(isChrome);
+
+
+navigator.keyboard.lock();
+
+function fullscreen() {
+  var isInFullScreen = (document.fullscreenElement && document.fullscreenElement !== null) ||
+      (document.webkitFullscreenElement && document.webkitFullscreenElement !== null) ||
+      (document.mozFullScreenElement && document.mozFullScreenElement !== null) ||
+      (document.msFullscreenElement && document.msFullscreenElement !== null);
+
+  var docElm = document.documentElement;
+  if (!isInFullScreen) {
+      if (docElm.requestFullscreen) {
+          docElm.requestFullscreen();
+      } else if (docElm.mozRequestFullScreen) {
+          docElm.mozRequestFullScreen();
+      } else if (docElm.webkitRequestFullScreen) {
+          docElm.webkitRequestFullScreen();
+      } else if (docElm.msRequestFullscreen) {
+          docElm.msRequestFullscreen();
+      }
+      
+      if (docElm.exitFullscreen) {
+        //document.exitFullscreen();
+          alert('close');
+      } else if (docElm.webkitExitFullscreen) {
+          //document.webkitExitFullscreen();
+          alert('close');
+      } else if (docElm.mozCancelFullScreen) {
+          //document.mozCancelFullScreen();
+      } else if (docElm.msExitFullscreen) {
+          //document.msExitFullscreen();
+          alert('close');
+      }
+  }
+      
+}
+
+
+var elem = document.body; // Make the body go full screen.
+//fullscreen(elem);
+
+
+
 var currentTab = 0; // Current tab is set to be the first tab (0)
 showTab(currentTab); // Display the current tab
+
+function opentab(numb){
+  var my,tabu = document.getElementsByClassName("tab");
+  fixStepIndicator(numb);
+  for (var my = 0; my <= tabu.length; my++){
+      if(tabu[my].attributes.style.nodeValue == "display: block;"){
+     //   console.log(`ok ${my} block`);
+        tabu[my].style.display = "none";
+        tabu[numb].style.display = "block";
+      }
+   // console.log(`ok ${my} block`);
+  }
+  
+  
+  
+}
 
 function showTab(n) {
   // This function will display the specified tab of the form...
@@ -50,20 +135,63 @@ function validateForm() {
   // This function deals with validation of the form fields
   var x, y, i, valid = true;
   x = document.getElementsByClassName("tab");
-  y = x[currentTab].getElementsByTagName("input");
-  // A loop that checks every input field in the current tab:
-  for (i = 0; i < y.length; i++) {
-    // If a field is empty...
-    if (y[i].value == "") {
-      // add an "invalid" class to the field:
-      document.getElementsByClassName("step")[currentTab].className += " invalid";
-      // and set the current valid status to false
-      valid = true;
+  //y = x[currentTab].getElementsByClassName("quest_type");
+  //z = [currentTab].getElementsByClassName("question");
+  y = document.querySelectorAll('.quest_type')[currentTab];
+  if(y.value == 'normal_mcq'){
+    var normal_mcq_checbox = document.getElementsByName('normal_mcq');
+    var formValid = false;
+    
+    var i = 0;
+    while (!formValid && i < normal_mcq_checbox.length) {
+        if (normal_mcq_checbox[i].checked) formValid = true;
+        i++;      
     }
+
+    if (!formValid){
+      document.getElementsByClassName("step")[currentTab].className += " invalid";
+    }
+  
+  }else if(y.value == 'multiselect'){
+    var normal_mcq_checbox = document.getElementsByName('multiselectoption');
+    var formValid = false;
+    
+    var i = 0;
+    while (!formValid && i < normal_mcq_checbox.length) {
+        if (normal_mcq_checbox[i].checked) formValid = true;
+        i++;      
+    }
+
+    if (!formValid){
+      document.getElementsByClassName("step")[currentTab].className += " invalid";
+    }else{
+      document.getElementsByClassName("step")[currentTab].className.replace(" invalid", "");
+    }
+  }else if(y.value == 'qa_question'){
+    var textarea = x[currentTab].getElementsByTagName("qa_question");
+    var formValid = false;
+    if(textarea.value == ''){
+      formValid = true;
+    }
+
+    if(!formValid){
+      document.getElementsByClassName("step")[currentTab].className += " invalid";
+    }else{
+      document.getElementsByClassName("step")[currentTab].className.replace(" invalid", "");
+    }
+
   }
-  // If the valid status is true, mark the step as finished and valid:
-  if (valid) {
+  
+  valid = true;
+  if (formValid) {
+    
+    // if(document.getElementsByClassName("step")[currentTab].classList.contains('invalid')) {
+    //   document.getElementsByClassName("step")[currentTab].className.replace(" invalid", "");
+    // }
+    
+
     document.getElementsByClassName("step")[currentTab].className += " finish";
+
   }
   return valid; // return the valid status
 }
@@ -79,46 +207,42 @@ function fixStepIndicator(n) {
 }
 
 
+Timer();
+
+function Timer() {
+  var deadline = new Date("Aug. 7, 2021, 00:30:00");
+  var newDateObj = new Date();
+  newDateObj.setTime(deadline.getTime() + ($('#durations').val() * 60 * 1000));
+  //console.log(newDateObj);
+  var x = setInterval(function() {
+  Startexam = deadline.getTime() <= new Date().getTime();
+  if(Startexam){
+  var now = new Date().getTime();
+  var t = newDateObj - now;
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+  var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((t % (1000 * 60)) / 1000);
+  document.querySelector("time").innerHTML = hours + ":" + minutes + ":" + seconds;
+      if (t < 0) {
+          clearInterval(x);
+          $('#ms').submit();
+          navigator.keyboard.unlock();
+          document.querySelector("time").innerHTML = 0 + ":" + 0 + ":" + 0;
+      }
+      }else{
+        console.log("Not started yet");
+  }
+  }, 1000);
+}
 
 
-    (function () {
-
-      var timeElement, eventTime, currentTime, duration, interval, intervalId;
-    
-      interval = 1000; // 1 second
-      startTime = $('#startTime').val();
-      endTime = $('#endTime').val();
-      // get time element
-      timeElement = document.querySelector("time");
-      // calculate difference between two times
-      eventTime = moment.tz(endTime, "Asia/Jakarta");
-      // based on time set in user's computer time / OS
-      currentTime = moment.tz(startTime, "Asia/Jakarta");
-      // get duration between two times
-      duration = moment.duration(eventTime.diff(currentTime));
-    
-      // loop to countdown every 1 second
-      setInterval(function() {
-        // get updated duration
-        duration = moment.duration(duration - interval, 'milliseconds');
-    
-        // if duration is >= 0
-        if (duration.asSeconds() <= 0) {
-          clearInterval(intervalId);
-          // hide the countdown element
-          $('#msform').submit();
-          timeElement.classList.add("hidden");
-        } else {
-          // otherwise, show the updated countdown
-          timeElement.innerText =  duration.hours() + ":" + duration.minutes() + ":" + duration.seconds()
-        }
-      }, interval);
-    
-    }());
 
     var NoQuestion = document.querySelectorAll('.tab');
 
-    $('#progress').append('<button id="progressbtn1" type="button" class="step btn btn-primary btn-sm border">1</button>');
-    for (let index = 2; index <= NoQuestion.length; index++) {
-      $('#progress').append('<button id="progressbtn'+index+'" type="button" class="step btn btn-light btn-sm border">'+index+'</button>');
+    $('#progress').append('<button id="progressbtn1" type="button" class="step btn btn-light btn-sm border active" onclick="opentab(0)">1</button>');
+    for (let index = 2; index < NoQuestion.length; index++) {
+      forClickableIndicator = index -1;
+      //NoQuestion.splice(index, 1);
+      $('#progress').append(`<button id="progressbtn${index}" type="button" class="step btn btn-light btn-sm border" onclick="opentab(${forClickableIndicator})">${index}</button>`);
     }
