5471f3a1eb5cff0dfc0bbfb409bd7b6d0fc6dfc6
Ayush Kumar <ayushkr08@gmail.com>
2021-10-24 15:41:09 +0530

    admin update

--- physics/admin.py
+++ physics/admin.py
@@ -4,14 +4,17 @@ from .models import chapter, course, onlinecontent, thought
 
 class chapterAdmin(admin.ModelAdmin):
     list_display = ('chapterId','chapterName','orderBy')
+    list_display_links = ('chapterId', 'chapterName')
 
 class courseAdmin(admin.ModelAdmin):
     list_display = ('courseId','topicName','chapterName','orderBy')
     list_filter = ('chapterName',)
+    list_display_links=('courseId','topicName')
 
 class onlinecontentAdmin(admin.ModelAdmin):
     list_display = ('contentId','title','topic','dataType','orderBy')
     list_filter = ('topic',)
+    list_display_links=('contentId','title')
 
 
 admin.site.register(chapter, chapterAdmin)
