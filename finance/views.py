d4d8479c1a71cbe9b9aa4575b470ca9e87a7d1e1
Ayush Kumar <ayushkr08@gmail.com>
2021-11-26 07:19:48 +0530

    experiment on gateway

--- finance/views.py
+++ finance/views.py
@@ -86,10 +86,10 @@ def my_webhook_view(request):
     )
   except ValueError as e:
     # Invalid payload
-    return HttpResponse(status=400)
+    return HttpResponse(status=401)
   except stripe.error.SignatureVerificationError as e:
     # Invalid signature
-    return HttpResponse(status=400)
+    return HttpResponse(status=402)
 
   # Handle the event
   if event.type == 'payment_intent.succeeded':
