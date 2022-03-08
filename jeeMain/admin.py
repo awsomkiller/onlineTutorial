2819cbab11c37963e85ce70dbd59de05c969155a
Ayush Kumar <ayushkr08@gmail.com>
2022-03-08 20:12:48 +0530

    server de liye changes

--- jeeMain/admin.py
+++ jeeMain/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from jeeMain.models import qa_question, question
-# Register your models here.
+# from jeeMain.models import qa_question, question
+# # Register your models here.
 
-admin.site.register(question)
-admin.site.register(qa_question)
+# admin.site.register(question)
+# admin.site.register(qa_question)
--- jeeMain/models.py
+++ jeeMain/models.py
@@ -4,26 +4,26 @@ from accounts.models import subscriptionplan
 from tinymce.models import HTMLField
 
 # Create your models here.
-class question(models.Model):
-    id = models.AutoField(primary_key=True)
-    chapter = models.ForeignKey(chapter, on_delete=models.PROTECT)
-    title = models.CharField(max_length=50)
-    description = HTMLField()
-    option1 = models.CharField(max_length=100)
-    option2 = models.CharField(max_length=100)
-    option3 = models.CharField(max_length=100)
-    option4 = models.CharField(max_length=100)
-    answer = models.CharField(max_length=100)
-    comprehension = models.BooleanField()
-    multiselect = models.BooleanField()
+# class question(models.Model):
+#     id = models.AutoField(primary_key=True)
+#     chapter = models.ForeignKey(chapter, on_delete=models.PROTECT)
+#     title = models.CharField(max_length=50)
+#     description = HTMLField()
+#     option1 = models.CharField(max_length=100)
+#     option2 = models.CharField(max_length=100)
+#     option3 = models.CharField(max_length=100)
+#     option4 = models.CharField(max_length=100)
+#     answer = models.CharField(max_length=100)
+#     comprehension = models.BooleanField()
+#     multiselect = models.BooleanField()
 
-class qa_question(models.Model):
-    id = models.AutoField(primary_key=True, editable=True)
-    title = models.CharField(max_length=50)
-    questiondescription = HTMLField()
-    answer =models.TextField(max_length=100)
-    def __str__(self):
-        return self.title
+# class qa_question(models.Model):
+#     id = models.AutoField(primary_key=True, editable=True)
+#     title = models.CharField(max_length=50)
+#     questiondescription = HTMLField()
+#     answer =models.TextField(max_length=100)
+#     def __str__(self):
+#         return self.title
 
 # class exam_portal(models.Model):
 #     id = models.AutoField(primary_key=True, editable=True)
